3b3accbe3107e9e328301facc0bf980f5f75d5b2
Peter Jakubek <pjakubek@student.tgm.ac.at>
2017-09-12 14:11:21 +0200

    UPDATE contact.html
    baustelle^10

---
+++ assets/php/formsender.php
@@ -0,0 +1,33 @@
+<?php
+$errors = '';
+$myemail = 'peter.jakubek@outlook.com';//<-----Put Your email address here.
+if(empty($_POST['email_address']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Contact form submission: $subject";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $email \n ".
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: contact-form-thank-you.html');
+print('alles gut');
+}
+?>
