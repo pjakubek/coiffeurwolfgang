b237e69b49451c6a47812bdf0b788d4bdd5057f0
Peter Jakubek <pjakubek@student.tgm.ac.at>
2017-09-13 20:43:16 +0200

    Auto stash before merge of "petrus" and "origin/master"

--- README.md
+++ README.md
@@ -3,20 +3,20 @@ Website Coiffeur Wolfgang
 
 #### Todo
 
-- [ ] Home
-- [ ] About
+- [x] Home
+- [x] About
 - [x] Preise
 - [x] Impressum
 - [x] Kontakt (Formular, Google Maps)
 - [ ] Terminverwaltung
 - [ ] Deutsch/Englisch
 - [ ] Stylesheet ersetzen/ergänzen
-- [ ] Navbar/Footer ändern (Footer ganz unten? Kontakt/Impressum nicht sichtbar)
+- [x] Navbar/Footer ändern (Footer ganz unten? Kontakt/Impressum nicht sichtbar)
 - [ ] Email senden Kontaktformular
 - [ ] Bewerbungsformular (Dateien hochladen)
-- [ ] Home Seite Farbe navbar (eigene Navbar fuer Index)
+- [x] Home Seite Farbe navbar (eigene Navbar fuer Index)
 - [x] Oeffnungszeiten Tabelle schaut seltsam aus (Petrus fast-fix)
-- [ ] Verlinkungen!
+- [x] Verlinkungen!
 
 
 #### Aufteilung
--- assets/php/formsender.php
+++ assets/php/formsender.php
@@ -1,33 +1,41 @@
 <?php
-$errors = '';
-$myemail = 'peter.jakubek@outlook.com';//<-----Put Your email address here.
-if(empty($_POST['email_address']) ||
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-$email_address = $_POST['email_address'];
-$message = $_POST['message'];
-$subject = $_POST['subject'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
 
-if( empty($errors))
-{
-$to = $myemail;
-$email_subject = "Contact form submission: $subject";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $email \n ".
-"Email: $email_address\n Message \n $message";
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-//redirect to the 'thank you' page
-header('Location: ../../de/contact.html');
-print('alles gut');
+use PHPMailer-master\src\PHPMailer.php;
+use PHPMailer-master\src\Exception.php;
+
+
+
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp-mail.outlook.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'peter.jakubek@outlook.com'};                 // SMTP username
+    $mail->Password = 'PeterJ11';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom($Username, 'Kunde');
+    $mail->addAddress('peter.jakubek@outlook.com', 'Joe User');     // Add a recipient
+
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  //  $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+  //  $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['message'];
+  //  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
 ?>
